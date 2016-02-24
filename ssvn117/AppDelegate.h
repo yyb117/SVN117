bd61bc3fb1044619d50af4632a6aed64b9768b6e
echo012 <echo012@bogon>
2016-02-24 10:45:38 +0800

    Initial Commit

---
+++ ssvn117/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ssvn117
+//
+//  Created by echo012 on 16/2/24.
+//  Copyright © 2016年 echo012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
