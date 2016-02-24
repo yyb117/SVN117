2ae1dc3384a22e33f4aa437e7f4d9af4643972d3
yb117 <1171727303@q.com>
2016-02-24 14:40:58 +0800

    666

--- ssvn117/ViewController.m
+++ ssvn117/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"333");
+    NSLog(@"33333");
 }
 
 - (void)didReceiveMemoryWarning {
